23a4e3afa75518a7247a3fc33fbba1f893eb54d7
1364223806 <18638141292@163.com>
2018-08-07 12:01:50 +0800

    13

--- compmgr-dao/src/main/java/edu/zut/cs/rolepower/model/Function.java
+++ compmgr-dao/src/main/java/edu/zut/cs/rolepower/model/Function.java
@@ -5,5 +5,23 @@ package edu.zut.cs.rolepower.model;
  * 2018年8月6日上午10:28:55
  */
 public class Function {
-
+	private Integer id;
+	private String function;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFunction() {
+		return function;
+	}
+	public void setFunction(String function) {
+		this.function = function;
+	}
+	@Override
+	public String toString() {
+		return "Function [id=" + id + ", function=" + function + "]";
+	}
+	
 }
