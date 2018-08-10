5e5739420a461a48ad9584d90d7f338ac9663c76
Bircheoods <951793132@qq.com>
2018-08-10 15:20:42 +0800

    dao层，日志

--- compmgr-dao/src/main/java/edu/zut/cs/user/dao/TeacherMapper.java
+++ compmgr-dao/src/main/java/edu/zut/cs/user/dao/TeacherMapper.java
@@ -10,10 +10,16 @@ import java.util.List;
  * @date: 2018-08-03 15:06
  */
 public interface TeacherMapper {
-
+    //获取所有老师信息
 	public List<Teacher> getAllTeacher();
-
+    //通过Id查询老师姓名
 	public String getNameById(Integer id);
-	
+	//通过Id查询老师密码
 	public String getPasswordById(Integer id);
+	//插入一条老师信息，返回状态值
+	public Integer insertTeacher(Teacher teacher);
+	//删除指定老师id的一条记录，返回状态值
+//	public Integer deleteTeacherById(Integer id);
+	//通过指定Id获取老师信息
+	public List<Teacher> getOneTeacherById(Integer id);
 }
