6ce8651d96cad61dcae58cd3f695fcfe77985c98
邹过 <18790058116@163.com>
2018-08-21 16:15:43 +0800

    '增加密码MD5加密验证，并修改数据库内的明文字段'

--- compmgr-service/src/main/java/edu/zut/cs/login/service/impl/LoginServiceImpl.java
+++ compmgr-service/src/main/java/edu/zut/cs/login/service/impl/LoginServiceImpl.java
@@ -1,6 +1,7 @@
 package edu.zut.cs.login.service.impl;
 
 import edu.zut.cs.login.service.LoginService;
+import edu.zut.cs.tools.Md5;
 import edu.zut.cs.user.dao.TeacherMapper;
 import edu.zut.cs.user.model.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,8 @@ public class LoginServiceImpl implements LoginService {
      */
     @Override
     public boolean isTruePassword(Integer teacherId,String password) {
-        if (password.equals(teacherMapper.getPasswordById(teacherId)))
+        String encodeStr = new Md5().lock(password);
+        if (encodeStr.equals(teacherMapper.getPasswordById(teacherId)))
             return true;
         return false;
     }
