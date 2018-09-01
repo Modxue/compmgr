f6cae683b4025f1b69710bd49f761c2e504e6251
1364223806 <18638141292@163.com>
2018-09-01 16:02:33 +0800

    改写权限功能后台代码

--- compmgr-dao/src/main/java/edu/zut/cs/rolepower/dao/RoleMapper.java
+++ compmgr-dao/src/main/java/edu/zut/cs/rolepower/dao/RoleMapper.java
@@ -4,29 +4,28 @@ import java.util.List;
 
 import edu.zut.cs.rolepower.model.Role;
 import edu.zut.cs.user.model.Teacher;
+import org.springframework.stereotype.Repository;
 
 /**
- * 
- * @author UML
- *
+ * @author GZL
+ * 2018年8月7日下午3:16:15
  */
 
+@Repository
 public interface RoleMapper {
-	
-	public String getDescriptionById(Integer id);
-	
-	public String getRoleNameById(Integer id);
 
 	//插入一条角色信息
-	public Integer insertRole(Role role);
+	public Integer insert(Role role);
 
 	//通过角色名字查询同名数量
-	public Integer selectAllSameName(String roleName);
-	
-	/**
-	 * @author GZL
-	 * 2018年8月7日下午3:16:15
-	 */
+	public Integer selectByRoleName(String roleName);
+
+	// 查询所有角色
+	List<Role> selectAll();
+
+	// 根据角色id更改记录
+	int update(Role role);
+
     // 查询角色下的教师
     List<Teacher> selectTeacher(Integer roleId);
 
@@ -35,4 +34,10 @@ public interface RoleMapper {
 
     // 删除一条角色记录
     int delete(Integer roleId);
+
+	// 为此角色添加教师
+	int insertTeacher(Integer roleId, Integer teacherId);
+
+	// 删除此角色下教师
+	int deleteTeacher(Integer roleId, Integer teacherId);
 }
--- compmgr-web/src/main/java/edu/zut/cs/rolepower/controller/RoleController.java
+++ compmgr-web/src/main/java/edu/zut/cs/rolepower/controller/RoleController.java
@@ -1,46 +1,100 @@
 package edu.zut.cs.rolepower.controller;
 
 import com.alibaba.fastjson.JSON;
+import edu.zut.cs.rolepower.model.Role;
 import edu.zut.cs.rolepower.service.RoleAllocationService;
+import edu.zut.cs.user.model.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @packge_name: edu.zut.cs.rolepower.controller
- * @author: 薛二伟
+ * @author: 薛二伟,郭志良
  * @Description:
  * @Date: 2018/8/22 20:10
  */
-@Controller
-@RequestMapping(value = "/role")
+@RestController
+@RequestMapping(value = "/role", produces = "application/json;charset=utf-8")
 public class RoleController {
 
     @Autowired
     RoleAllocationService roleAllocationService;
 
-    @PostMapping(value = "/addrole",produces = "application/json;charset=utf-8")
-    public @ResponseBody String getUserInfo(@RequestBody String message)throws Exception{
-        System.out.println("前台传的参数：");
+    // 查询所有角色
+    @RequestMapping("/search")
+    public String selectAll() {
+        List<Role> result = roleAllocationService.selectAll();
+        return JSON.toJSONString(result);
+    }
 
-        Map map = JSON.parseObject(message);
-        System.out.println(map);
-        String roleName= (String) map.get("name");
-        String description= (String) map.get("description");
-        if (!roleAllocationService.isHaveSameName(roleName)){
-            if (roleAllocationService.isAddRole(roleName,description)){
-                System.out.println("hahahahahahahahahahah!");
-                return JSON.toJSONString(1);
-            }
-            System.out.println("heheheheheheheh!");
-            return JSON.toJSONString(-1);
+    // 根据角色id更改角色信息
+    @RequestMapping("/update")
+    public String update(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        Integer id = Integer.parseInt((String) map.get("id"));
+        String roleName = (String) map.get("roleName");
+        String description = (String) map.get("description");
+        Role role = new Role(id, roleName, description);
+        int result = roleAllocationService.update(role);
+        return JSON.toJSONString(result);
+    }
+
+    // 插入一条角色记录
+    @RequestMapping("/add")
+    public String insert(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        String roleName = (String) map.get("roleName");
+        String description = (String) map.get("description");
+        Role role = new Role(roleName, description);
+        int result = roleAllocationService.insert(role);
+        return JSON.toJSONString(result);
+    }
+
+    // 查询角色下的教师
+    @RequestMapping("/searchteacher")
+    public String selectTeacher(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        Integer roleId = Integer.parseInt((String) map.get("id"));
+        List<Teacher> result = roleAllocationService.selectTeacher(roleId);
+        return JSON.toJSONString(result);
+    }
+
+    // 删除一条角色记录
+    @RequestMapping("/delete")
+    public String delete(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        Integer roleId = Integer.parseInt((String) map.get("id"));
+        int result = roleAllocationService.delete(roleId);
+        return JSON.toJSONString(result);
+    }
+
+    // 为角色添加教师
+    @RequestMapping("/insertTeacher")
+    public String insertTeacher(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        Integer roleId = Integer.parseInt((String) map.get("roleId"));
+        String teacherIdStr = (String) map.get("teacherId");
+        String[] teacherIdArr = teacherIdStr.split(",");  // 将字符串切割成数组
+        Integer[] teacherId = new Integer[teacherIdArr.length];
+        for (int i = 0, j = teacherIdArr.length; i < j; i++) {  // 将String数组转化为Integer数组
+            teacherId[i] = Integer.parseInt(teacherIdArr[i]);
         }
+        int result = roleAllocationService.insertTeacher(roleId, teacherId);
+        System.out.println(result);
+        return JSON.toJSONString(result);
+    }
 
-        return JSON.toJSONString(0);
+    // 角色删除教师
+    @RequestMapping("/deleteTeacher")
+    public String deleteTeacher(@RequestBody String str) {
+        Map map = JSON.parseObject(str);
+        Integer roleId = Integer.parseInt((String) map.get("roleId"));
+        Integer teacherId = Integer.parseInt((String) map.get("teacherId"));
+        int result = roleAllocationService.deleteTeacher(roleId, teacherId);
+        return JSON.toJSONString(result);
     }
 }
