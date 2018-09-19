7f64cfdaa9c421cdb286bfcdb3c82dcec8a5fe20
邹过 <18790058116@163.com>
2018-09-19 19:28:57 +0800

    Excel测试

--- compmgr-service/src/test/java/edu/zut/cs/teacher/service/TeacherServiceTest.java
+++ compmgr-service/src/test/java/edu/zut/cs/teacher/service/TeacherServiceTest.java
@@ -1,8 +1,14 @@
 package edu.zut.cs.teacher.service;
 
+import com.alibaba.druid.util.StringUtils;
 import edu.zut.cs.tools.Md5;
 import edu.zut.cs.user.dao.TeacherMapper;
 import edu.zut.cs.user.model.Teacher;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +16,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 侯捷
@@ -55,7 +66,7 @@ public class TeacherServiceTest {
         else
             result=0;
     }
-     @Test
+    @Test
     public  void insertTeacherTest(){
          Integer result=-1;
          Teacher teacher1=new Teacher();
@@ -70,4 +81,39 @@ public class TeacherServiceTest {
          teacherMapper.insertTeacher(teacher1);
          result=0;
      }
+    @Test
+    public void readExcelTest() throws Exception {
+
+        List<Teacher> list = new ArrayList<>();
+        InputStream input = TeacherServiceTest.class.getResourceAsStream("/t_teacher_excel.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(input);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            Teacher teacher = new Teacher();
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                Cell cell = row.getCell(j);
+                if(cell!=null)
+                {
+                    cell.setCellType(CellType.STRING);
+                    String value = row.getCell(j).getStringCellValue().trim();
+                    if (j == 1)
+                        teacher.setTeacherNum(value);
+                    if (j == 2)
+                        teacher.setTeacherName(value);
+                    if (j == 3)
+                        teacher.setTeacherEmail(value);
+                    if (j == 4)
+                        teacher.setPassword(value);
+                }
+
+            }
+            list.add(teacher);
+        }
+        for (Teacher teacher : list) {
+            System.out.println(teacher);
+        }
+        System.out.println(list.size());
+
+    }
 }
