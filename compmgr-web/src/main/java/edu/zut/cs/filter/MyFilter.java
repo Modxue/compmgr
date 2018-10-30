6fa38a383075ff4fd8394f6086ed055924b53d37
邹过 <18790058116@163.com>
2018-10-30 18:00:37 +0800

    '对企业微信进行测试'

---
+++ compmgr-web/src/main/java/edu/zut/cs/filter/MyFilter.java
@@ -0,0 +1,82 @@
+//package edu.zut.cs.filter;
+//
+//import edu.zut.cs.user.model.Teacher;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Arrays;
+//import java.util.List;
+//
+///**
+// * create_by Intellij IDEA
+// *
+// * @author zouguo0212@
+// * @package_name edu.zut.cs.filter
+// * @description
+// * @date 2018/10/19 15:42
+// */
+//public class MyFilter implements Filter {
+//
+//    /** 要检查的 session 的名称 */
+//    private String sessionKey;
+//
+//    /** 检查不通过时，转发的URL */
+//    private String redirectUrl;
+//
+//    //不检查的url
+//    private String uncheckedUrls;
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.out.println("SessionFilter的启动");
+//        sessionKey= filterConfig.getInitParameter("sessionKey");
+//        redirectUrl= filterConfig.getInitParameter("redirectUrl");
+//        uncheckedUrls = filterConfig.getInitParameter("uncheckedUrls");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        if (sessionKey==null||sessionKey.equals("")){
+//            filterChain.doFilter(servletRequest,servletResponse);
+//            return;
+//        }
+//
+//        // 获得在下面代码中要用的request,response,session对象
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//
+//        //1.获取请求URL
+//        String servletPath = request.getServletPath();
+//
+//
+//        //2.检测1中获取的servletPath是否为不需要检测的URl中的一个.若是,放行
+//        List<String> urls = Arrays.asList(uncheckedUrls.split(","));
+//        if (urls.contains(servletPath)
+//                ||servletPath.endsWith(".js")
+//                ||servletPath.endsWith(".css")
+//                ||servletPath.endsWith(".jpg")
+//                ||servletPath.endsWith(".png")) {
+////            System.out.println("放行当前的访问路径："+servletPath);
+////            System.out.println("Session中的teacher信息："+teacher);
+//            filterChain.doFilter(servletRequest,servletResponse);
+//        }else {
+//            //3.从session中获取SessionKey对应值,若值不存在,则重定向到redirectUrl
+//            Object user = request.getSession().getAttribute("teacher");
+//
+//            if (user != null) {
+////                Teacher teacher = (Teacher) user;
+////                System.out.println("UserNot Null的teacher信息："+teacher);
+//                filterChain.doFilter(servletRequest,servletResponse);
+//            }else{
+//                    response.sendRedirect(redirectUrl);
+//                }
+//            }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("SessionFilter的销毁");
+//    }
+//}
---
+++ compmgr-web/src/main/java/edu/zut/cs/wechat/controller/WeChatController.java
@@ -0,0 +1,125 @@
+package edu.zut.cs.wechat.controller;
+
+import com.alibaba.fastjson.JSON;
+import edu.zut.cs.teacher.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * create_by Intellij IDEA
+ *
+ * @author zouguo0212@
+ * @package_name edu.zut.cs.wechat.controller
+ * @description
+ * @date 2018/10/30 15:31
+ */
+@RequestMapping(value = "/weChat")
+@Controller
+public class WeChatController {
+
+
+    @Autowired
+    TeacherService teacherService;
+
+    private final static String secrect = "Qp1lX4eq31TCypYqIVtOSRljfDka3SADpvKKy9xeHto";
+    private final static String corpid = "ww1f4196b8f50b704b";
+
+
+    @RequestMapping(value = "/login",produces = "application/json;charset=utf-8")
+    public String init(HttpServletRequest request,
+            @RequestParam(value = "code")String code,@RequestParam(value = "state") String state) {
+        String accessTokenJson = sendGet("https://qyapi.weixin.qq.com/cgi-bin/gettoken",
+                "corpid="+corpid+"&corpsecret="+secrect);
+        Map map = JSON.parseObject(accessTokenJson);
+        String accessToken = (String) map.get("access_token");
+        String infoJson = sendGet("https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo",
+                "access_token="+accessToken+"&code="+code);
+        map = JSON.parseObject(infoJson);
+        String userId = (String) map.get("UserId");
+//        Teacher teacher = teacherService.getTeacherByUserId(userId);
+        HttpSession session = request.getSession();
+//        session.setAttribute("teacher",teacher);
+        session.setAttribute("userId",userId);
+        session.setAttribute("code",code);
+        session.setAttribute("accessTokenJson",accessTokenJson);
+        session.setAttribute("infoJson",infoJson);
+        return "redirect:/html/weChat.html";
+    }
+
+    @GetMapping(value = "/getUserId",produces = "application/json;charset=utf-8")
+    public @ResponseBody
+    String getUserId(HttpSession session){
+        String[] str = new String[3];
+        str[0] = (String) session.getAttribute("code");
+        str[1] = (String) session.getAttribute("accessTokenJson");
+        str[2] = (String) session.getAttribute("infoJson");
+        return JSON.toJSONString(str);
+    }
+
+
+    /**
+     * 向指定URL发送GET方法的请求
+     *
+     * @param url
+     *            发送请求的URL
+     * @param param
+     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return URL 所代表远程资源的响应结果
+     */
+    public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+}
